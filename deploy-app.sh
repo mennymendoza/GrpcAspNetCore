9e52f27ab705228cd189d13c7329e9201aaee76c
Juan Mendoza <jmen9575@gmail.com>
2023-05-17 09:31:34 -0700

    commented out protoc command and provided steps to make sure it runs correctly

--- deploy-app.sh
+++ deploy-app.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# If you want to change the greet.proto file, you need to re-compile the gRPC-web client libraries
-# Run the command 'npm install -g protoc-gen-grpc-web' to globally install the tool and then run the following command
 export GRPC_OUTPUT_PATH=./ClientApp/src/grpc-compiled
-protoc -I=./Protos greet.proto --js_out=import_style=commonjs,binary:$GRPC_OUTPUT_PATH --grpc-web_out=import_style=typescript,mode=grpcweb:$GRPC_OUTPUT_PATH
+# If you want to change the greet.proto file, you need to re-compile the gRPC-web client libraries
+# Follow the following steps:
+# 1. Run the command 'sudo npm install -g protoc-gen-grpc-web' to globally install the grpc tool
+# 2. Uncomment the following command and re-run this script
+# protoc -I=./Protos greet.proto --js_out=import_style=commonjs,binary:$GRPC_OUTPUT_PATH --grpc-web_out=import_style=typescript,mode=grpcweb:$GRPC_OUTPUT_PATH
 
 # Builds frontend from source
 cd ./ClientApp/
