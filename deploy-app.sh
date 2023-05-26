40e687c6818b970d8f8c74cce21c292a5fc91f04
Juan Mendoza <jmen9575@gmail.com>
2023-05-26 14:19:40 -0700

    swapped React configuration to use create-react-app for convenience

--- deploy-app.sh
+++ deploy-app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export GRPC_OUTPUT_PATH=./ClientApp/src/grpc-compiled
+export GRPC_OUTPUT_PATH=./client-app/src/grpc-compiled
 # If you want to change the greet.proto file, you need to re-compile the gRPC-web client libraries
 # Follow the following steps:
 # 1. Run the command 'sudo npm install -g protoc-gen-grpc-web' to globally install the grpc tool
@@ -8,14 +8,14 @@ export GRPC_OUTPUT_PATH=./ClientApp/src/grpc-compiled
 # protoc -I=./Protos greet.proto --js_out=import_style=commonjs,binary:$GRPC_OUTPUT_PATH --grpc-web_out=import_style=typescript,mode=grpcweb:$GRPC_OUTPUT_PATH
 
 # Builds frontend from source
-cd ./ClientApp/
+cd ./client-app/
 npm install .
 npm run build
 
 # Copies frontend to wwwroot, where ASP.NET Core will look for static files
 cd ../
 rm -r ./wwwroot/*
-cp -r ./ClientApp/build/* ./wwwroot/
+cp -r ./client-app/build/* ./wwwroot/
 
 # Publishes backend
 dotnet publish --configuration Release
