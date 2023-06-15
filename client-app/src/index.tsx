a5dcc9f1ff1efd36b1d267fdb4ed4485fa71a551
Juan Mendoza <juan.mendoza@promachbuilt.com>
2023-06-15 09:24:16 -0700

    added custom styling examples

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { ThemeProvider } from '@emotion/react';
+import { createTheme } from '@mui/material';
+import { green, purple } from '@mui/material/colors';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import './custom.css';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
