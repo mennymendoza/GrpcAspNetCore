1c21117e3928236b25df835057e9a8fd442ecbf8
jmendoza <juan.mendoza@promachbuilt.com>
2024-02-26 10:39:10 -0800

    Updated deploy-app.sh script with new vite dist path and updated proto compilation to new protobuf-ts task

--- deploy-app.sh
+++ deploy-app.sh
@@ -2,18 +2,20 @@
 
 export GRPC_OUTPUT_PATH=./client-app/src/grpc-compiled
 
-# Recompiles greet.proto file into Typescript/Javascript
-protoc -I=./Protos greet.proto --js_out=import_style=commonjs:$GRPC_OUTPUT_PATH --grpc-web_out=import_style=typescript,mode=grpcwebtext:$GRPC_OUTPUT_PATH
-
-# Builds frontend from source
+# Installs npm dependencies
 cd ./client-app/
 npm install .
+
+# Recompiles greet.proto file into Typescript/Javascript
+npm run gen:protobuf-ts
+
+# Builds frontend from source
 npm run build
 
 # Copies frontend to wwwroot, where ASP.NET Core will look for static files
 cd ../
 rm -r ./wwwroot/*
-cp -r ./client-app/build/* ./wwwroot/
+cp -r ./client-app/dist/* ./wwwroot/
 
 # Publishes backend
 dotnet publish --configuration Release
